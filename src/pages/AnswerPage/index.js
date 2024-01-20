3bf093cd6aa73bdfa2770d375938f8b7dc2c6a13
clyde <clydehan0401@gmail.com>
2024-01-21 03:59:34 +0900

    feat: 카드 컨테이너 내부 작업

--- src/pages/AnswerPage/index.js
+++ src/pages/AnswerPage/index.js
@@ -14,25 +14,83 @@ const AnswerPage = () => {
               />
             </div>
             <div>
-              <img src="/assets/images/profile.svg" alt="프로필 이미지" />
+              <img
+                src="/assets/images/profile.svg"
+                alt="프로필 이미지"
+                className="header-profile-image"
+              />
             </div>
             <h2 className="profile-title">아초는고양이</h2>
             <div className="button-wrap">
               <button type="button" className="sns-button link-color">
-                <img src="/assets/images/Link.svg" alt="링크 복사하기" />
+                <img src="/assets/images/link.svg" alt="링크 복사하기" />
               </button>
 
               <button type="button" className="sns-button kakaotalk-color">
-                <img src="/assets/images/Kakaotalk.svg" alt="카카오톡 공유" />
+                <img src="/assets/images/kakaotalk.svg" alt="카카오톡 공유" />
               </button>
 
               <button type="button" className="sns-button facebook-color">
-                <img src="/assets/images/Facebook.svg" alt="페이스북 공유" />
+                <img src="/assets/images/facebook.svg" alt="페이스북 공유" />
               </button>
             </div>
           </div>
         </div>
       </header>
+      <main>
+        <article>
+          <button type="button" className="delete-button">
+            삭제하기
+          </button>
+          <div className="question-list-wrap">
+            <div className="list-title-wrap">
+              <img src="/assets/images/Messages.svg" alt="메시지 아이콘" />
+              <h3 className="list-title">1개의 질문이 있습니다.</h3>
+            </div>
+            <div className="question-card">
+              <div className="card-navigation">
+                <div className="answer-status">답변 완료</div>
+                <img src="/assets/images/kebab.svg" alt="더보기 버튼" />
+              </div>
+              <div className="card-title-wrap">
+                <h4 className="write-date">질문 2주전</h4>
+                <h3 className="card-title">좋아하는 동물은?</h3>
+              </div>
+              <div className="card-answer-section">
+                <div className="card-profile-wrap">
+                  <img
+                    src="/assets/images/profile.svg"
+                    alt="프로필 이미지"
+                    className="card-profile-image"
+                  />
+                </div>
+                <div className="answer-input-area">
+                  <div className="answer-input-wrap">
+                    <h3 className="answer-profile-name">아초는고양이</h3>
+                    <input
+                      placeholder="답변을 입력해주세요."
+                      className="answer-input"
+                    />
+                    <button type="button" className="answer-button">
+                      답변완료
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="reaction-section">
+                <div className="reaction-like">
+                  <img src="/assets/images/thumbsUp.svg" alt="좋아요 버튼" />
+                  <h4>좋아요</h4>
+                </div>
+                <div className="reaction-hate">
+                  <img src="/assets/images/thumbsDown.svg" alt="싫어요 버튼" />
+                  <h4>싫어요</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </main>
     </AnswerPageContainer>
   );
 };
--- src/pages/AnswerPage/style.js
+++ src/pages/AnswerPage/style.js
@@ -7,6 +7,8 @@ const AnswerPageContainer = styled.div`
   background-size: cover;
   background-color: var(--Grayscale-20);
 
+  // 헤더 섹션 //
+
   .header-background-image {
     position: relative;
     width: 100%;
@@ -33,6 +35,11 @@ const AnswerPageContainer = styled.div`
     height: 67px;
     margin-top: 50px;
     margin-bottom: 12px;
+  }
+
+  .header-profile-image {
+    width: 136px;
+    height: 136px;
   }
 
   .profile-title {
@@ -71,6 +78,229 @@ const AnswerPageContainer = styled.div`
 
   .facebook-color {
     background-color: var(--Blue-50);
+  }
+
+  // 메인 섹션 //
+
+  .delete-button {
+    color: var(--Grayscale-10);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 25px;
+    margin-top: 145px;
+
+    border-radius: 200px;
+    border: none;
+    background: var(--Brown-40);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+    display: flex;
+    width: 100px;
+    height: 35px;
+    padding: 12px 24px;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+  }
+
+  .question-list-wrap {
+    display: inline-flex;
+    padding: 16px;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+
+    border-radius: 16px;
+    border: 1px solid var(--Brown-30);
+    background: var(--Brown-10);
+  }
+
+  .list-title-wrap {
+    display: flex;
+  }
+
+  .list-title {
+    color: var(--Brown-40);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Actor;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 25px;
+  }
+
+  .card-navigation {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .question-card {
+    display: flex;
+    padding: 32px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 32px;
+    align-self: stretch;
+
+    border-radius: 16px;
+    background: var(--Grayscale-10);
+
+    box-shadow: 0px 4px 4px 0px rgba(140, 140, 140, 0.25);
+  }
+
+  .answer-status {
+    color: var(--Brown-40);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 18px;
+
+    display: flex;
+    padding: 4px 12px;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 8px;
+    border: 1px solid var(--Brown-40);
+    background: var(--Grayscale-10);
+
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 18px;
+  }
+
+  .write-date {
+    color: var(--Grayscale-40);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 18px;
+  }
+
+  .card-answer-section {
+    display: flex;
+  }
+
+  .card-title {
+    color: var(--Grayscale-60);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Actor;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+  }
+
+  .card-profile-image {
+    width: 48px;
+    height: 48px;
+  }
+
+  .answer-input-area {
+    display: flex;
+  }
+
+  .answer-input-wrap {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .answer-profile-name {
+    color: var(--Grayscale-60);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Actor;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+  }
+
+  .answer-input {
+    height: 186px;
+    padding: 16px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+
+    border: none;
+    border-radius: 8px;
+    background: var(--Grayscale-30);
+  }
+
+  .answer-button {
+    color: var(--Grayscale-10);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px;
+
+    display: flex;
+    height: 46px;
+    padding: 12px 24px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+
+    border: none;
+    border-radius: 8px;
+    background: var(--Brown-40);
+  }
+  .reaction-section {
+    display: flex;
+    gap: 32px;
+    border-top: 1px solid var(--Grayscale-30);
+
+    h4 {
+      color: var(--Grayscale-40);
+      font-feature-settings:
+        'clig' off,
+        'liga' off;
+      font-family: Pretendard;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 18px;
+
+      margin-left: 6px;
+    }
+  }
+
+  .reaction-like {
+    display: flex;
+  }
+
+  .reaction-hate {
+    display: flex;
   }
 `;
 
