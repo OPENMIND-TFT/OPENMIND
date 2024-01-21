6f899dc84bf72fe3842b0b34577a3a35489b62cd
haeyong9701 <haeyong9701@gmail.com>
2024-01-21 23:47:02 +0900

    feat: 메인, 질문 작성하기 버튼 추가

--- src/pages/PheedPage/style.js
+++ src/pages/PheedPage/style.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 
 const PheedPageContainer = styled.div`
+  position: relative;
+
   .pheed-page-header {
     position: relative;
     height: 234px;
     background-color: var(--Grayscale-10);
   }
+
   .header-section {
     position: absolute;
     bottom: 0;
@@ -18,54 +21,248 @@ const PheedPageContainer = styled.div`
     align-items: center;
     flex-direction: column;
     gap: 12px;
+
+    .logo {
+      width: 170px;
+      height: 67px;
+    }
+
+    .profile {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 12px;
+    }
+
+    .profile-name {
+      font-size: 3.2rem;
+    }
+
+    .profile-share-icons {
+      display: flex;
+      gap: 12px;
+    }
+
+    .profile-share-link-box {
+      width: 40px;
+      height: 40px;
+      border-radius: 9999px;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .profile-share-link-logo {
+      width: 18px;
+      height: 18px;
+    }
+
+    .profile-share-link-box.brown {
+      background-color: var(--Brown-40);
+    }
+
+    .profile-share-link-box.yellow {
+      background-color: var(--Yellow-50);
+    }
+
+    .profile-share-link-box.blue {
+      background-color: var(--Blue-50);
+    }
   }
 
-  .header-logo {
-    width: 170px;
-    height: 67px;
+  .pheed-page-main {
+    display: flex;
+    justify-content: center;
   }
 
-  .header-profile {
+  .question-list-container {
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 12px;
+    margin-top: 189px;
+    margin-bottom: 136px;
+    width: 716px;
+    min-height: 330px;
+    background-color: var(--Brown-10);
+    border: 1px solid var(--Brown-30);
+    border-radius: 16px;
+    padding: 16px;
+
+    .title-box {
+      display: flex;
+      gap: 8px;
+    }
+
+    .title {
+      font-family: Actor;
+      color: var(--Brown-40);
+      font-size: 2rem;
+      margin-bottom: 16px;
+    }
+
+    .title-image {
+      background-image: url('/assets/images/Messages.svg');
+      width: 24px;
+      height: 24px;
+    }
   }
 
-  .profile-name {
-    font-size: 3.2rem;
-  }
-
-  .profile-share-icons {
+  .question-list {
+    width: 100%;
     display: flex;
-    gap: 12px;
+    flex-direction: column;
+    gap: 20px;
+
+    .question-answer-box {
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+      border-radius: 16px;
+      background: var(--Grayscale-10);
+      box-shadow: 0px 4px 4px 0px rgba(140, 140, 140, 0.25);
+      width: 100%;
+      min-height: 243px;
+      padding: 32px;
+    }
+
+    .answer {
+      width: 76px;
+      height: 26px;
+      padding: 4px 11px;
+      font-size: 1.4rem;
+      font-weight: 500;
+      color: var(--Brown-40, #542f1a);
+      border-radius: 8px;
+      border: 1px solid var(--Brown-40);
+      background: var(--Grayscale-10);
+    }
+
+    .answer.none {
+      width: 61px;
+      border-color: var(--Grayscale-40);
+      color: var(--Grayscale-40);
+    }
+
+    .question-box {
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+    }
+
+    .question-title-box {
+      font-size: 1.4rem;
+      color: var(--Grayscale-40);
+      margin-bottom: 4px;
+    }
+
+    .question-content {
+      font-size: 1.8rem;
+      font-family: Actor;
+    }
+
+    .answer-box {
+      display: flex;
+      gap: 12px;
+    }
+
+    .answer-profile-image {
+      width: 48px;
+      height: 48px;
+    }
+
+    .answer-title-box {
+      display: flex;
+      align-items: center;
+      margin-bottom: 4px;
+    }
+
+    .answer-profile-name {
+      font-size: 1.8rem;
+      font-family: Actor;
+      margin-right: 8px;
+      line-height: 24px;
+    }
+
+    .answer-time-line {
+      font-size: 1.4rem;
+      color: var(--Grayscale-40);
+    }
+
+    .answer-content {
+      font-size: 1.6rem;
+      line-height: 22px;
+    }
+
+    .answer-content.refuse {
+      color: var(--Red-50);
+    }
+
+    .reaction-button-box {
+      display: flex;
+      align-items: flex-end;
+      border-top: 1px solid var(--Grayscale-30);
+      gap: 32px;
+      height: 43px;
+    }
+
+    .like-button-box,
+    .dislike-button-box {
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      cursor: pointer;
+      font-size: 1.4rem;
+      color: var(--Grayscale-40);
+    }
+
+    .like-button-box.on {
+      color: var(--Blue-50);
+      .tumbs-up-image {
+        filter: invert(36%) sepia(32%) saturate(6200%) hue-rotate(203deg)
+          brightness(100%) contrast(91%);
+      }
+    }
+
+    .dislike-button-box.on {
+      color: var(--Red-50);
+      .tumbs-up-image {
+        filter: invert(20%) sepia(24%) saturate(6385%) hue-rotate(344deg)
+          brightness(111%) contrast(79%);
+      }
+    }
+
+    .tumbs-up-image {
+      background: url('/assets/images/thumbs-up.svg');
+    }
+
+    .tumbs-down-image {
+      background: url('/assets/images/thumbs-down.svg');
+    }
+
+    .tumbs-up-image,
+    .tumbs-down-image {
+      width: 16px;
+      height: 16px;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
   }
 
-  .profile-share-link-box {
-    width: 40px;
-    height: 40px;
-    border-radius: 9999px;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .profile-share-link-box.brown {
-    background-color: var(--Brown-40);
-  }
-
-  .profile-share-link-box.yellow {
-    background-color: var(--Yellow-50);
-  }
-
-  .profile-share-link-box.blue {
-    background-color: var(--Blue-50);
-  }
-
-  .profile-link-icon {
-    width: 18px;
-    height: 18px;
+  .question-write-button {
+    position: fixed;
+    z-index: 1;
+    bottom: 24px;
+    right: 24px;
+    width: 208px;
+    height: 54px;
+    padding: 12px 24px;
+    border-radius: 200px;
+    font-size: 2rem;
+    color: var(--Grayscale-10);
+    background: var(--Brown-40);
+    border: 0;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   }
 `;
 
