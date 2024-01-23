bfb767c42a23d9d8b1bcc03036dd87a56294e362
심은주 <nyaknya@naver.com>
2024-01-23 14:41:56 +0900

    fix: 404페이지 export default로 변경

--- src/pages/NotFoundPage/style.js
+++ src/pages/NotFoundPage/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const NotFoundContainer = styled.div`
+const NotFoundContainer = styled.div`
   width: 100%;
   height: 100vh;
   background: url(/assets/images/paperBackground.png) no-repeat;
@@ -58,3 +58,5 @@ export const NotFoundContainer = styled.div`
     }
   }
 `;
+
+export default NotFoundContainer;
