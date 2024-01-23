2a74bae497f43268690f226b616c4e6368029943
haeyong9701 <haeyong9701@gmail.com>
2024-01-24 02:22:43 +0900

    chore: 임시로 한 파일에서 컴포넌트 분리

--- src/pages/QuestionPage/index.js
+++ src/pages/QuestionPage/index.js
@@ -5,7 +5,7 @@ import QuestionPageContainer from './style';
 
 const API_BASE_URL = 'https://openmind-api.vercel.app/3-5';
 
-// TODO 임시 ID. useState으로 나중에 관리하기. App.js post/id값 바꾸기.
+// TODO 임시 ID. useParams로 동적인 경로 만들어주기.
 const USER_ID = 2805;
 
 const getUser = async () => {
@@ -26,9 +26,135 @@ const getUserQuestions = async () => {
   return response.json();
 };
 
+const ReactionButtonBox = ({ question }) => {
+  return (
+    <div className="reaction-button-box">
+      <div className="like-button-box on">
+        <figure className="tumbs-up-image" />
+        <span className="like">좋아요</span>
+        <span className="like-count">{question.like}</span>
+      </div>
+      <div className="dislike-button-box">
+        <figure className="tumbs-down-image" />
+        <span className="dislike">싫어요</span>
+        <span className="dislike-count">{question.dislike}</span>
+      </div>
+    </div>
+  );
+};
+
+const QuestionItem = ({ user, question }) => {
+  return (
+    <section className="question-answer-box answer-complete">
+      {question.answer ? (
+        <div className="answer complete">답변 완료</div>
+      ) : (
+        <div className="answer none">미답변</div>
+      )}
+      <div className="question-box">
+        <div className="question-title-box">
+          <span className="question-title">질문 · </span>
+          <span className="question-time-line">
+            {getElapsedTime(question.createdAt)}
+          </span>
+        </div>
+        <span className="question-content">{question.content}</span>
+      </div>
+
+      {question.answer ? (
+        <div className="answer-box">
+          <img
+            src={user.imageSource}
+            className="answer-profile-image"
+            alt="프로필 이미지"
+          />
+          <div className="answer-content-box">
+            <div className="answer-title-box">
+              <span className="answer-profile-name">{user.name}</span>
+              <span className="answer-time-line">
+                {getElapsedTime(question.answer.createdAt)}
+              </span>
+            </div>
+            {question.answer.isRejected ? (
+              <span className="answer-content refuse">답변 거절</span>
+            ) : (
+              <span className="answer-content">{question.answer.content}</span>
+            )}
+          </div>
+        </div>
+      ) : null}
+      <ReactionButtonBox question={question} />
+    </section>
+  );
+};
+
+const QuestionList = ({ user, questions }) => {
+  return (
+    <div className="question-list">
+      {questions.length &&
+        questions.map(question => (
+          <QuestionItem key={question.id} user={user} question={question} />
+        ))}
+    </div>
+  );
+};
+
+const ProfileShareIcons = () => {
+  return (
+    <div className="profile-share-icons">
+      <div className="profile-share-link-box brown">
+        <img
+          src="/assets/images/Link.svg"
+          className="profile-share-link-logo"
+          alt="링크 공유하기 기능이 있는 로고"
+        />
+      </div>
+      <div className="profile-share-link-box yellow">
+        <img
+          src="/assets/images/Kakaotalk.svg"
+          className="profile-share-link-logo Kakaotalk-logo"
+          alt="카카오톡으로 공유하기 기능이 있는 카카오톡 로고"
+        />
+      </div>
+      <div className="profile-share-link-box blue">
+        <img
+          src="/assets/images/Facebook.svg"
+          className="profile-share-link-logo Facebook-logo"
+          alt="페이스북으로 공유하기 기능이 있는 페이스북 로고"
+        />
+      </div>
+    </div>
+  );
+};
+
+const QuestionHeader = ({ user }) => {
+  return (
+    <header className="questions-page-header">
+      <section className="header-section">
+        <Link to="/">
+          <img
+            src="/assets/images/logo.png"
+            className="logo"
+            alt="OpenMind 서비스 로고"
+          />
+        </Link>
+        <section className="profile">
+          <img
+            src={user.imageSource}
+            className="header-profile-image"
+            alt="프로필 이미지"
+          />
+          <span className="profile-name">{user.name}</span>
+          <ProfileShareIcons />
+        </section>
+      </section>
+    </header>
+  );
+};
+
 const QuestionPage = () => {
-  const [questions, setQuestions] = useState([]);
   const [user, setUser] = useState([]);
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,49 +170,7 @@ const QuestionPage = () => {
 
   return (
     <QuestionPageContainer>
-      <header className="questions-page-header">
-        <section className="header-section">
-          <Link to="/">
-            <img
-              src="/assets/images/logo.png"
-              className="logo"
-              alt="OpenMind 서비스 로고"
-            />
-          </Link>
-          <section className="profile">
-            <img
-              src={user.imageSource}
-              className="header-profile-image"
-              alt="프로필 이미지"
-            />
-            <span className="profile-name">{user.name}</span>
-
-            <div className="profile-share-icons">
-              <div className="profile-share-link-box brown">
-                <img
-                  src="/assets/images/Link.svg"
-                  className="profile-share-link-logo"
-                  alt="링크 공유하기 기능이 있는 로고"
-                />
-              </div>
-              <div className="profile-share-link-box yellow">
-                <img
-                  src="/assets/images/Kakaotalk.svg"
-                  className="profile-share-link-logo Kakaotalk-logo"
-                  alt="카카오톡으로 공유하기 기능이 있는 카카오톡 로고"
-                />
-              </div>
-              <div className="profile-share-link-box blue">
-                <img
-                  src="/assets/images/Facebook.svg"
-                  className="profile-share-link-logo Facebook-logo"
-                  alt="페이스북으로 공유하기 기능이 있는 페이스북 로고"
-                />
-              </div>
-            </div>
-          </section>
-        </section>
-      </header>
+      <QuestionHeader user={user} />
 
       <main className="questions-page-main">
         <article className="question-list-container">
@@ -98,73 +182,7 @@ const QuestionPage = () => {
           </div>
           {/* 질문이 없는 경우 no-question-image 활성화
           <figure className="no-question-image" /> */}
-
-          <div className="question-list">
-            {questions.length &&
-              questions.map(question => (
-                <section
-                  key={question.id}
-                  className="question-answer-box answer-complete"
-                >
-                  {question.answer ? (
-                    <div className="answer complete">답변 완료</div>
-                  ) : (
-                    <div className="answer none">미답변</div>
-                  )}
-                  <div className="question-box">
-                    <div className="question-title-box">
-                      <span className="question-title">질문 · </span>
-                      <span className="question-time-line">
-                        {getElapsedTime(question.createdAt)}
-                      </span>
-                    </div>
-                    <span className="question-content">{question.content}</span>
-                  </div>
-
-                  {question.answer ? (
-                    <div className="answer-box">
-                      <img
-                        src={user.imageSource}
-                        className="answer-profile-image"
-                        alt="프로필 이미지"
-                      />
-                      <div className="answer-content-box">
-                        <div className="answer-title-box">
-                          <span className="answer-profile-name">
-                            {user.name}
-                          </span>
-                          <span className="answer-time-line">
-                            {getElapsedTime(question.answer.createdAt)}
-                          </span>
-                        </div>
-                        {question.answer.isRejected ? (
-                          <span className="answer-content refuse">
-                            답변 거절
-                          </span>
-                        ) : (
-                          <span className="answer-content">
-                            {question.answer.content}
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                  ) : null}
-
-                  <div className="reaction-button-box">
-                    <div className="like-button-box on">
-                      <figure className="tumbs-up-image" />
-                      <span className="like">좋아요</span>
-                      <span className="like-count">{question.like}</span>
-                    </div>
-                    <div className="dislike-button-box">
-                      <figure className="tumbs-down-image" />
-                      <span className="dislike">싫어요</span>
-                      <span className="dislike-count">{question.dislike}</span>
-                    </div>
-                  </div>
-                </section>
-              ))}
-          </div>
+          <QuestionList user={user} questions={questions} />
         </article>
       </main>
 
