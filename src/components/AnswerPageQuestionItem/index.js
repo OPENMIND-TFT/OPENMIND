f8778967bbbed27ccca8cc965f5c0f00f52f3e23
clyde <clydehan0401@gmail.com>
2024-01-25 03:07:58 +0900

    feat: 답변하기 기능, 답변출력 기능

--- src/components/AnswerPageQuestionItem/index.js
+++ src/components/AnswerPageQuestionItem/index.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import getElapsedTime from '../../utils/getElapsedTime';
 import QuestionContainer from './style';
 
 const QuestionItem = ({ user, question }) => {
+  const [content, setContent] = useState('');
+  const { id } = question;
+
+  const submitAnswer = async () => {
+    const response = await fetch(
+      `https://openmind-api.vercel.app/3-5/questions/${id}/answers/`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content,
+          isRejected: 'false',
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error('답변을 전송하는데 실패했습니다.');
+    }
+
+    setContent('');
+  };
+
   return (
     <QuestionContainer>
       <div className="question-card">
@@ -22,37 +48,95 @@ const QuestionItem = ({ user, question }) => {
           </span>
           <h3 className="card-title">{question.content}</h3>
         </div>
-        <div className="card-answer-section">
-          <div className="card-profile-wrap">
-            <img
-              src={user.imageSource}
-              alt="프로필 이미지"
-              className="card-profile-image"
-            />
-          </div>
-          <div className="answer-input-area">
-            <div className="answer-input-wrap">
-              <h3 className="answer-profile-name">{user.name}</h3>
-              <textarea
-                placeholder="답변을 입력해주세요."
-                className="answer-textarea"
-              />
-              <button type="button" className="answer-button">
-                답변완료
-              </button>
+
+        {question.answer ? (
+          <>
+            <div className="card-answer-section">
+              <div className="card-profile-wrap">
+                <img
+                  src={user.imageSource}
+                  alt="프로필 이미지"
+                  className="card-profile-image"
+                />
+              </div>
+              <div className="answer-input-area">
+                <div className="answer-area-wrap">
+                  <div className="answer-profile-wrap">
+                    <h3 className="answer-profile-name">{user.name}</h3>
+                    <h4 className="answer-date">
+                      {getElapsedTime(question.answer.createdAt)}
+                    </h4>
+                  </div>
+                  <p className="reply-font">{question.answer.content}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="reaction-section">
-          <div className="reaction-like">
-            <img src="/assets/images/thumbsUp.svg" alt="좋아요 버튼" />
-            <h4>좋아요</h4>
-          </div>
-          <div className="reaction-hate">
-            <img src="/assets/images/thumbsDown.svg" alt="싫어요 버튼" />
-            <h4>싫어요</h4>
-          </div>
-        </div>
+
+            <footer className="card-footer-section">
+              <div className="reaction-section">
+                <div className="reaction-like">
+                  <img src="/assets/images/thumbsUp.svg" alt="좋아요 버튼" />
+                  <h4>좋아요</h4>
+                </div>
+                <div className="reaction-hate">
+                  <img src="/assets/images/thumbsDown.svg" alt="싫어요 버튼" />
+                  <h4>싫어요</h4>
+                </div>
+              </div>
+              <button className="edit-button" type="button">
+                <img src="/assets/images/edit.svg" alt="수정 버튼" />
+                수정하기
+              </button>
+            </footer>
+          </>
+        ) : (
+          <>
+            <div className="card-answer-section">
+              <div className="card-profile-wrap">
+                <img
+                  src={user.imageSource}
+                  alt="프로필 이미지"
+                  className="card-profile-image"
+                />
+              </div>
+              <div className="answer-input-area">
+                <div className="answer-input-wrap">
+                  <h3 className="answer-profile-name">{user.name}</h3>
+                  {/* //여기 답변기능 추가해야됨 */}
+                  <form>
+                    <textarea
+                      onChange={e => setContent(e.target.value)}
+                      value={content}
+                      placeholder="답변을 입력해주세요."
+                      className="answer-textarea"
+                    />
+                    <button
+                      type="submit"
+                      className="answer-button"
+                      onClick={submitAnswer}
+                    >
+                      답변완료
+                    </button>
+                  </form>
+                  {/* //여기 답변기능 추가해야됨 */}
+                </div>
+              </div>
+            </div>
+
+            <footer className="card-footer-section">
+              <div className="reaction-section">
+                <div className="reaction-like">
+                  <img src="/assets/images/thumbsUp.svg" alt="좋아요 버튼" />
+                  <h4>좋아요</h4>
+                </div>
+                <div className="reaction-hate">
+                  <img src="/assets/images/thumbsDown.svg" alt="싫어요 버튼" />
+                  <h4>싫어요</h4>
+                </div>
+              </div>
+            </footer>
+          </>
+        )}
       </div>
     </QuestionContainer>
   );
--- src/components/AnswerPageQuestionItem/style.js
+++ src/components/AnswerPageQuestionItem/style.js
@@ -112,6 +112,25 @@ const QuestionContainer = styled.div`
     line-height: 24px;
   }
 
+  .answer-date {
+    color: var(--Grayscale-40);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 18px;
+  }
+
+  .reply-font {
+    color: var(--Grayscale-60, #000);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-size: 1.6rem;
+    line-height: 22px;
+  }
+
   .answer-textarea {
     width: 100%;
     height: 186px;
@@ -156,10 +175,11 @@ const QuestionContainer = styled.div`
     background: var(--Brown-40);
   }
 
-  .reaction-section {
+  .card-footer-section {
     display: flex;
-    gap: 32px;
     border-top: 1px solid var(--Grayscale-30);
+    justify-content: space-between;
+    align-items: center;
 
     h4 {
       color: var(--Grayscale-40);
@@ -169,9 +189,13 @@ const QuestionContainer = styled.div`
       font-size: 1.4rem;
       font-weight: 500;
       line-height: 18px;
-
       margin-left: 6px;
     }
+  }
+
+  .reaction-section {
+    display: flex;
+    gap: 32px;
   }
 
   .reaction-like {
@@ -182,6 +206,45 @@ const QuestionContainer = styled.div`
   .reaction-hate {
     display: flex;
     margin-top: 24px;
+  }
+
+  .edit-button {
+    margin-top: 24px;
+    gap: 8px;
+    color: var(--Grayscale-50);
+    font-feature-settings:
+      'clig' off,
+      'liga' off;
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 18px;
+    display: flex;
+    width: 103px;
+    padding: 10px 15px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    border: 1px solid var(--Grayscale-30);
+    background: var(--Grayscale-10);
+    box-shadow: 0px 4px 4px 0px rgba(140, 140, 140, 0.25);
+
+    img {
+      width: 14px;
+      height: 14px;
+    }
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1124px) {
+    .edit-button {
+      display: none;
+    }
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 767px) {
+    .edit-button {
+      display: none;
+    }
   }
 `;
 
