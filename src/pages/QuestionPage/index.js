a23e5e6f67b03f071a77202dcb77518f9d1da2e5
haeyong9701 <haeyong9701@gmail.com>
2024-01-25 21:16:30 +0900

    test: PR 잘되는지 확인

--- src/pages/QuestionPage/index.js
+++ src/pages/QuestionPage/index.js
@@ -4,7 +4,7 @@ import getElapsedTime from '../../utils/getElapsedTime';
 import QuestionPageContainer from './style';
 
 const API_BASE_URL = 'https://openmind-api.vercel.app/3-5';
-
+// PR 잘되나?
 const getUser = async userId => {
   const response = await fetch(`${API_BASE_URL}/subjects/${userId}/`);
   if (!response.ok) {
