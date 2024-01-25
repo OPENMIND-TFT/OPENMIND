0458c68fed3717a20b73a85d5a1dac8c067a761b
clyde <clydehan0401@gmail.com>
2024-01-25 19:31:31 +0900

    fix: 헤더 css 누락 부분 수정

--- src/components/AnswerPageQuestionHeader/index.js
+++ src/components/AnswerPageQuestionHeader/index.js
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
-import HeaderContainer from './style';
+import AnswerPageHeaderContainer from './style';
+import ShareButtonBar from '../ShareButtonBar';
 
 const AnswerPageQuestionHeader = ({ user }) => {
   return (
-    <HeaderContainer>
+    <AnswerPageHeaderContainer>
       <div className="header-background-image">
         <div className="header-profile-wrap">
           <Link to="/">
@@ -21,22 +22,10 @@ const AnswerPageQuestionHeader = ({ user }) => {
             />
           </div>
           <h2 className="profile-title">{user.name}</h2>
-          <div className="button-wrap">
-            <button type="button" className="sns-button link-color">
-              <img src="/assets/images/link.svg" alt="링크 복사하기" />
-            </button>
-
-            <button type="button" className="sns-button kakaotalk-color">
-              <img src="/assets/images/kakaotalk.svg" alt="카카오톡 공유" />
-            </button>
-
-            <button type="button" className="sns-button facebook-color">
-              <img src="/assets/images/facebook.svg" alt="페이스북 공유" />
-            </button>
-          </div>
+          <ShareButtonBar />
         </div>
       </div>
-    </HeaderContainer>
+    </AnswerPageHeaderContainer>
   );
 };
 
