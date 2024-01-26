286cba6e9d37b5c6911801aac4eee456d2f7528a
Park JunYong <dyden119@gmail.com>
2024-01-26 17:35:15 +0900

    feat: 좋아요, 싫어요

--- src/components/ReactionButtonBox/index.js
+++ src/components/ReactionButtonBox/index.js
@@ -1,18 +1,61 @@
+import { useState } from 'react';
 import ReactionButtonBoxContainer from './style';
 
 const ReactionButtonBox = ({ question, style }) => {
+  const URL = `https://openmind-api.vercel.app/3-5/questions/${question.id}/reaction/`;
+  const [likeCount, setLikeCount] = useState(question.like);
+  const [dislikeCount, setDislikeCount] = useState(question.dislike);
+
+  const handleLike = async () => {
+    await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        type: 'like',
+      }),
+    });
+    setLikeCount(likeCount + 1);
+  };
+
+  const handleDislike = async () => {
+    await fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        type: 'dislike',
+      }),
+    });
+    setDislikeCount(dislikeCount + 1);
+  };
+
   return (
     <ReactionButtonBoxContainer>
       <div className="reaction-button-box" style={style}>
-        <div className="like-button-box on">
+        <div
+          className={`like-button-box ${likeCount !== 0 ? 'on' : ''}`}
+          onClick={handleLike}
+          onKeyDown={() => {}}
+          role="presentation"
+        >
           <figure className="tumbs-up-image" />
           <span className="like">좋아요</span>
-          <span className="like-count">{question.like}</span>
+          {likeCount !== 0 && <span className="like-count">{likeCount}</span>}
         </div>
-        <div className="dislike-button-box">
+        <div
+          className={`dislike-button-box ${dislikeCount !== 0 ? 'on' : ''}`}
+          onClick={handleDislike}
+          onKeyDown={() => {}}
+          role="presentation"
+        >
           <figure className="tumbs-down-image" />
           <span className="dislike">싫어요</span>
-          <span className="dislike-count">{question.dislike}</span>
+          {dislikeCount !== 0 && (
+            <span className="dislike-count">{dislikeCount}</span>
+          )}
         </div>
       </div>
     </ReactionButtonBoxContainer>
