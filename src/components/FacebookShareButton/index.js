5ede3f6f6cd0691f5fa3f46ae0efbb3170d19aa9
nyaknya <49646127+nyaknya@users.noreply.github.com>
2024-01-27 11:24:33 +0900

    feat: 링크공유기능 sns
    * fix: export 방식 변경
    
    * fix: 프리티어 작동에 오류가 떠 npm 재설치 작업
    
    * 페이스북 공유
    
    * feat: 카카오 공유 기능 배포 전 저장
    
    * chore: 빌드
    
    * fix: 배포 후 주소 지정
    
    * fix: 공유시 보이는 이미지 및 타이틀 수정
    
    * fix: 배포 404 오류 수정
    
    * fix: 공유시 뜨는 이미지 절대경로 설정
    
    * fix: 공유시 뜨는 이미지 절대경로 설정
    
    * fix: 공유시 뜨는 이미지 재수정
    
    * fix: 공유시 뜨는 이미지 최종 수정

--- src/components/FacebookShareButton/index.js
+++ src/components/FacebookShareButton/index.js
@@ -1,11 +1,14 @@
 import FacebookShareButtonStyled from './style';
 
 const FacebookShareButton = () => {
+  const handleFacebookShare = () => {
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
+    );
+  };
+
   return (
-    <FacebookShareButtonStyled
-      type="button"
-      className="sns-button facebook-color"
-    >
+    <FacebookShareButtonStyled type="button" onClick={handleFacebookShare}>
       <img src="/assets/images/facebook.svg" alt="페이스북 공유" />
     </FacebookShareButtonStyled>
   );
