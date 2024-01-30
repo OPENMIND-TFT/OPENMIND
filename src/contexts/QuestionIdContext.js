fdd2b999ce1effc1afdbfd770a35911cc3a94f78
haeyong9701 <haeyong9701@gmail.com>
2024-01-30 11:47:47 +0900

    feat: 삭제모달

---
+++ src/contexts/QuestionIdContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const QuestionIdContext = createContext();
+
+export default QuestionIdContext;
--- src/pages/AnswerPage/index.js
+++ src/pages/AnswerPage/index.js
@@ -5,11 +5,16 @@ import AnswerPageContainer from './style';
 import QuestionHeader from '../../components/QuestionHeader';
 import getUserData from '../../api/getUserData';
 import deleteAll from '../../api/deleteAll';
+import DeleteModal from '../../components/DeleteModal';
+import DeleteModalContext from '../../contexts/DeleteModalContext';
+import QuestionIdContext from '../../contexts/QuestionIdContext';
 
 const AnswerPage = () => {
   const [user, setUser] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
+  const [isDeleteModal, setIsDeleteModal] = useState(false);
+  const [questionId, setQuestionId] = useState('');
 
   const fetchData = async () => {
     const responseUser = await getUserData(id);
@@ -26,24 +31,37 @@ const AnswerPage = () => {
   };
 
   return (
-    <AnswerPageContainer>
-      <QuestionHeader user={user} />
-      <main className="list-area">
-        <article className="article-container">
-          <div className="delete-button-wrap">
-            <button
-              type="button"
-              className="delete-button"
-              onClick={handleDeleteAll}
-            >
-              삭제하기
-            </button>
-          </div>
+    <QuestionIdContext.Provider value={setQuestionId}>
+      <DeleteModalContext.Provider value={setIsDeleteModal}>
+        <AnswerPageContainer>
+          <QuestionHeader user={user} />
+          <main className="list-area">
+            <article className="article-container">
+              <div className="delete-button-wrap">
+                <button
+                  type="button"
+                  className="delete-button"
+                  onClick={e => setIsDeleteModal(e.target.textContent)}
+                >
+                  삭제하기
+                </button>
+              </div>
 
-          <AnswerPageQuestionList user={user} id={id} />
-        </article>
-      </main>
-    </AnswerPageContainer>
+              <AnswerPageQuestionList user={user} id={id} />
+            </article>
+          </main>
+
+          {isDeleteModal && (
+            <DeleteModal
+              questionId={questionId}
+              isDeleteModal={isDeleteModal}
+              setIsDeleteModal={setIsDeleteModal}
+              handleDeleteAll={handleDeleteAll}
+            />
+          )}
+        </AnswerPageContainer>
+      </DeleteModalContext.Provider>
+    </QuestionIdContext.Provider>
   );
 };
 
